06b45dbb4aa816cfdbac8f8932e935440ecc70c0
marckii8888 <lvlarcus1997@gmail.com>
2020-04-30 00:15:47 +0800

    Changed more css

--- trelloapp/admin.py
+++ trelloapp/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
-from .models import Todo
+from .models import Todo, TaskList
 # Register your models here.
 
-
+class TaskListAdmin(admin.ModelAdmin):
+    list_display = ('title',)
 class TodoAdmin(admin.ModelAdmin):
     list_display = ('title', 'description', 'completed')
 
 admin.site.register(Todo, TodoAdmin)
-# admin.site.register(TaskList, TaskListAdmin)
+admin.site.register(TaskList, TaskListAdmin)
--- trelloapp/models.py
+++ trelloapp/models.py
@@ -2,15 +2,13 @@ from django.db import models
 
 # Create your models here.
 
-# class TaskList(models.Model):
-#     title = models.CharField(max_length=120)
-    
-#     def __str__(self):
-#         return self.title
+class TaskList(models.Model):
+    title = models.CharField(max_length=120)
+    def __str__(self):
+        return self.title
 
 class Todo(models.Model):
     title = models.CharField(max_length=120)
-    # task_list = models.ForeignKey(TaskList, on_delete=models.CASCADE)
     description = models.TextField()
     completed = models.BooleanField(default=False)
 
