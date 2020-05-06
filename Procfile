479b7217d56f93c7bbf82313e66a454d7976ab76
marckii8888 <lvlarcus1997@gmail.com>
2020-05-07 00:53:43 +0800

    trying to host

---
+++ Procfile
@@ -0,0 +1,2 @@
+release: python manage.py migrate
+web: gunicorn trello.wsgi --log-file -
--- trello/settings.py
+++ trello/settings.py
@@ -125,6 +125,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'my-app/build/static'),
 ]
